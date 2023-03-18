559b739e5e6907629aeb2f8d9c96f5e0fb3d8363
AnastasiaZhekina <yfcnzjkzdbnz09@gmail.com>
2023-03-19 02:54:59 +0500

    again

--- TwoNumbersCalculator.cpp
+++ TwoNumbersCalculator.cpp
@@ -9,9 +9,6 @@ class Calculator {
 public:
     Calculator() : num1(1), num2(1) {};
 
-    void setx1(int p_num1) { num1 = p_num1; }
-    void setx2(int p_num2) { num2 = p_num2; }
-
     double add() { return num1 + num2; }
     double multiply() { return num1 * num2; }
     double subtract_1_2() { return num1 - num2; }
@@ -19,12 +16,12 @@ public:
     double divide_1_2() { return num1 / num2; }
     double divide_2_1() { return num2 / num1; }
     bool set_num1(int n) {
-        if (n == 0) { cout << "Неверный ввод!" << endl; }
-        else { return true; }
+        if (n == 0) {return false; }
+        else { num1 = n;  return true; }
     }
     bool set_num2(int n) {
-        if (n == 0) { cout << "Неверный ввод!" << endl; }
-        else { return true; }
+        if (n == 0) { return false; }
+        else { num2 = n; return true; }
     }
 };
 int main()
@@ -39,19 +36,17 @@ int main()
         do {
             cout << "Введите число 1: ";
             cin >> n1;
-            number.set_num1(n1);
-        } while (n1 == 0);
-
-        number.setx1(n1);
-
+           if(number.set_num1(n1))
+               cout << "Неверный ввод!" << endl;
+        } while (number.set_num1(n1));
+           
         do {
             cout << "Введите число 2: ";
             cin >> n2;
-            number.set_num2(n2);
-        } while (n2 == 0);
-            
-        number.setx2(n2);
-       
+            if(!number.set_num2(n2))
+                cout << "Неверный ввод!" << endl;
+        } while (!number.set_num2(n2));
+
         cout << "num1 + num2 = " << number.add() << endl;
         cout << "num1 - num2 = " << number.subtract_1_2() << endl;;
         cout << "num2 - num1 = " << number.subtract_2_1() << endl;
